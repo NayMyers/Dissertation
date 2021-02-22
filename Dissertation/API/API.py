f00b8365f8c329422f31386bbab9529e38568797
Ryan Syme <33377621+NayMyers@users.noreply.github.com>
2021-02-22 02:00:25 +0000

    crude image upload working. Needs polish

--- Dissertation/API/API.py
+++ Dissertation/API/API.py
@@ -1,14 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
-import werkzeug
+import base64
 app = Flask(__name__)
 api = Api(app)
 
 # APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 image_upload_args = reqparse.RequestParser()
-image_upload_args.add_argument("imageName", type=str, help="Send Image Name", required=True)
-image_upload_args.add_argument("image", type=werkzeug.datastructures.FileStorage, help="Send Image")
 
 class HelloWorld(Resource):
     def post(self, name):
@@ -18,19 +16,13 @@ class HelloWorld(Resource):
         return {"data": "Hello World GET " + name}
 
 class Image(Resource):
-    def post(self):
-        args = image_upload_args.parse_args()
-        print(args)
-        return{"data": "IMAGE " + args['imageName'] + " UPLOADED"}
+    def put(self):
+        sentImage = request.files['image']
+        sentImage.save('testImageName3.png')
+        return{"data": "IMAGE " + 'sentimagename' + " UPLOADED"}
 
     def get(self, image):
         return{"data": "GET TEST SUCCESS " + image}
-
-    def put(self):
-        args = image_upload_args.parse_args()
-        print(args)
-        return{"data": "IMAGE " + args['imageName'] + " UPLOADED"}
-
 
 api.add_resource(HelloWorld, "/hello_world/<string:name>")
 api.add_resource(Image, "/upload_image")
