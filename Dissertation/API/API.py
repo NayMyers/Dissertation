7f8608fd60d2d70b24feeba82966decef863739d
Ryan Syme <33377621+NayMyers@users.noreply.github.com>
2021-02-22 05:36:49 +0000

    image upload now accepts image name and checks if correct format

--- Dissertation/API/API.py
+++ Dissertation/API/API.py
@@ -1,12 +1,11 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
-import base64
+from flask_restful import Api, Resource
+from datetime import datetime, date
+import re
 app = Flask(__name__)
 api = Api(app)
 
 # APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-image_upload_args = reqparse.RequestParser()
 
 class HelloWorld(Resource):
     def post(self, name):
@@ -17,9 +16,27 @@ class HelloWorld(Resource):
 
 class Image(Resource):
     def put(self):
+        responseMessage = "Invalid Request"
+        pngPat = r"\.png"
+        jpgPat = r"\.jpg"
+
+        now = datetime.now()
+        today = date.today()
+        currentDate = today.strftime("%y-%m-%d")
+        currentTime = now.strftime("%H-%M-%S")
+
+        sentImageName = request.form['imageName']
         sentImage = request.files['image']
-        sentImage.save('testImageName3.png')
-        return{"data": "IMAGE " + 'sentimagename' + " UPLOADED"}
+
+        pngCheck = re.search(pngPat, sentImageName)
+        jpgCheck = re.search(jpgPat, sentImageName)
+        if pngCheck == None and jpgCheck == None:
+            return{"data": "INVALID FILE FORMAT. ONLY jpg OR png"}
+
+        filetype = sentImageName.split('.')[-1]
+        sentImageFileName = currentDate + '-' + currentTime + '.' + filetype
+        sentImage.save(sentImageFileName)
+        return{"data": "IMAGE " + sentImageName + " UPLOADED"}
 
     def get(self, image):
         return{"data": "GET TEST SUCCESS " + image}
